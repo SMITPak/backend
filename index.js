94839c2242e95ad8d7e8b2d8639748fd7d711f92
SMITPak <smitwebandapp@gmail.com>
2025-07-01 23:13:41 +0500

    fa

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import router from "./Routes/index.js";
-import path from 'path'
+import path from "path";
+import mongoose from "./DB/index.js";
 
 const app = express();
 
@@ -9,19 +10,28 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.use('/images', express.static('images/'))
+app.use("/images", express.static("images/"));
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   const { key } = req?.query;
-//   if (key == 123) {
-//     next();
-//   }
-//   else{
-//     res.status(404).send({ status: 404,  message: 'You are not authorized user'})
-//   }
-// });
+app.use((req, res, next) => {
+  const { key } = req?.query;
+  if (key == 123) {
+    next();
+  }
+  else{
+    res.status(404).send({ status: 404,  message: 'You are not authorized user'})
+  }
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("Connection error:", error);
+});
+db.once("open", function () {
+  console.log("DB Connected");
+});
 
 app.use("/api", router);
 
