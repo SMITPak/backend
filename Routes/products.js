ad029480141145f0dbd4fe4004b31216d41f07b6
SMITPak <smitwebandapp@gmail.com>
2025-07-08 23:20:52 +0500

    fix

--- Routes/products.js
+++ Routes/products.js
@@ -5,9 +5,21 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const filters = req.query;
-    const products = await Products.find(filters);
-    return res.status(200).send({ status: 200, products });
+    const { limit = 10, page = 1, ...filters } = req.query;
+    const totalRecord = await Products.countDocuments();
+    const skipData = (page - 1) * parseInt(limit);
+    const products = await Products.find(filters).skip(skipData).limit(parseInt(limit));
+
+    return res.status(200).send({
+      status: 200,
+      products,
+      pagination: {
+        totalRecord,
+        totalPage: Math.ceil(totalRecord / parseInt(limit)),
+        limit: parseInt(limit),
+        page: parseInt(page),
+      },
+    });
   } catch (err) {
     return res.status(500).send({ status: 500, message: err.message });
   }
